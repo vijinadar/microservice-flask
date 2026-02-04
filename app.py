1d9eaa44d656baa9207dc7919d52a50e430b704e
vijinadar <g25ai1067@iitj.ac.in>
2026-02-04 09:07:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({
+        "message": "Hello from Python Flask Microservice"
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
